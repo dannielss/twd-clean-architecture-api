2fd279c06e043c8182b06445ed577641cbedeaec
Daniel <daniel_sousa.p@outlook.com>
2022-08-24 23:44:58 -0300

    chore: configure jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnviroment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  roots: [
+    '<rootDir>/src'
+  ]
+}
