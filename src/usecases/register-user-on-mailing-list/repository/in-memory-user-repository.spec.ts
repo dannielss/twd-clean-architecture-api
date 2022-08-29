35c328344a2ad044b1749c2a90a436848c8ae6dd
Daniel <daniel_sousa.p@outlook.com>
2022-08-29 00:03:00 -0300

    test: add, exists and findByEmail

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,14 @@ describe('In memory user repository', () => {
     const user = await userRepo.findUserByEmail('any@gmail.com')
     expect(user).toBeNull()
   })
+
+  it('should return user if it is not found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@gmail.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail(email)
+    expect(user).toStrictEqual({ name, email })
+  })
 })
