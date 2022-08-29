f690fc864d55b8d5e4fdfb9eb21d06cf4573c9bf
Daniel <daniel_sousa.p@outlook.com>
2022-08-29 00:02:52 -0300

    feat: add, exists and findByEmail

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -9,10 +9,19 @@ export class InMemoryUserRepository implements UserRepository {
   }
 
   async add (user: UserData): Promise<void> {
-
+    const exists = await this.exists(user)
+    if (!exists) {
+      this.#repository.push(user)
+    }
   }
 
   async findUserByEmail (email: string): Promise<UserData> {
+    const users = this.#repository.filter(user => user.email === email)
+
+    if (users.length > 0) {
+      return users[0]
+    }
+
     return null
   }
 
@@ -21,6 +30,12 @@ export class InMemoryUserRepository implements UserRepository {
   }
 
   async exists (user: UserData): Promise<boolean> {
+    const userExists = await this.findUserByEmail(user.email)
+
+    if (!userExists) {
+      return false
+    }
+
     return true
   }
 }
